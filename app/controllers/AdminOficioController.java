5e99612751cc91292b062f9dfd820119af9339d0
Eduardo Puente <epuente_72@yahoo.com>
2023-07-13 18:54:58 -0600

    Actualización

--- app/controllers/AdminOficioController.java
+++ app/controllers/AdminOficioController.java
@@ -96,113 +96,11 @@ public class AdminOficioController extends ControladorSeguroAdministrador {
 
 
 		//	aux.id=1931L;
-/*
-		List<Oficio> tempo = Oficio.find.order("id").findList();
-		Long idMax =  (tempo.get(tempo.size()-1).id)+1;
-		
-		
-		System.out.println("nuevo id de Oficio "+idMax);
-		
-		
-		Oficio nuevo = new Oficio();
 
-		
-		nuevo.id = idMax;
-		nuevo.oficio = aux.oficio;
-		nuevo.urremitente = aux.urremitente;
-		nuevo.descripcion = aux.descripcion;
-		nuevo.titulo = aux.titulo;
-		nuevo.fecharecibidoupev = aux.fecharecibidoupev;
-		nuevo.fecharemitente = aux.fecharemitente;
-		nuevo.observacion = aux.observacion;
-		
-		
-		System.out.println("servicios tam " + aux.servicios.size());
-		System.out.println("servicios " + aux.servicios);
-		System.out.println("servicios[0] " + aux.servicios.get(0));
-		System.out.println("servicios[0].servicio " + aux.servicios.get(0).servicio);
-		System.out.println("servicios[0].servicio.id " + aux.servicios.get(0).servicio.id);
-		
-		
-		nuevo.urremitente = UnidadResponsable.find.setId(aux.urremitente.id).findUnique();
-
-		List<OficioServicioSolicitado> tempo2 = OficioServicioSolicitado.find.order("id").findList();
-		Long idMax2 =  (tempo2.get(tempo2.size()-1).id)+1;
-
-		
-		for (int i = 0; i<aux.servicios.size(); i++) {
-			System.out.println("ser id "+i+" ------->"+aux.servicios.get(i).servicio.id);
-			OficioServicioSolicitado oss = new OficioServicioSolicitado();
-			oss.servicio = Servicio.find.setId(aux.servicios.get(i).servicio.id).findUnique();
-
-			oss.id = idMax2;
-			System.out.println("id para OficioServicioSolicitado "+idMax2);
-			nuevo.servicios.add(oss);
-			idMax2++;
-		}
-		
-		List<OficioProductorSolicitado> maxProdSol = OficioProductorSolicitado.find.order("id").findList();
-		Long idMaxProdSol = (maxProdSol.get(maxProdSol.size()-1).id)+1;
-		for (int i=0; i<aux.productoresSolicitados.size(); i++) {
-			OficioProductorSolicitado ops = new OficioProductorSolicitado();
-			ops.id=idMaxProdSol++;
-			ops.personal = Personal.find.setId(aux.productoresSolicitados.get(i).personal.id).findUnique();
-			nuevo.productoresSolicitados.add(ops);
-		}
-		
-		
-		nuevo.fechagrabaciones = aux.fechagrabaciones;
-		
-		List<OficioContacto> maxOfCont = OficioContacto.find.order("id").findList();
-		List<OficioContactoTelefono> maxOfContTel = OficioContactoTelefono.find.order("id").findList();
-		List<OficioContactoCorreo> maxOfContCorreo = OficioContactoCorreo.find.order("id").findList();
-		Long idMaxOfCont = (maxOfCont.get(maxOfCont.size()-1).id)+1;
-		Long idMaxOfContTel = (maxOfContTel.get(maxOfContTel.size()-1).id)+1;
-		Long idMaxOfContCorr = (maxOfContCorreo.get(maxOfContCorreo.size()-1).id)+1;
-		for ( OficioContacto ofCont  : aux.contactos) {
-			System.out.println( ofCont.nombre  );
-			ofCont.id=idMaxOfCont++;
-			
-			for (   OficioContactoTelefono tel  :  ofCont.telefonos ) {
-				System.out.println("     "+ tel.telefono );
-				tel.id = idMaxOfContTel++;
-			}
-			for (   OficioContactoCorreo correo  :  ofCont.correos ) {
-				System.out.println("     "+ correo.correo );
-				correo.id = idMaxOfContCorr++;
-			}
-		}
-		
-		
-		
-		for (  OficioContacto contacto :aux.contactos   ) {
-			
-		}
-		
-		
-		nuevo.contactos = aux.contactos;
-		
-		nuevo.observacion = aux.observacion;
-		
-		
-
-	//	aux.id = idMax;
-	//	aux.save();
-		
-		
-	//	nuevo.productoresSolicitados = aux.productoresSolicitados;
-
-		nuevo.save();
-		*/
 		aux.save();
 		aux.refresh();
 		retorno.put("estado", "ok");
 		retorno.put("id", aux.id);
-
-
-		//	flash("success", "Oficio " + oficiorForm.get().oficio + " se agregó");
-		//return list();
-		//	return redirect( routes.AdminOficioController.list());
 		return ok(retorno.toString());
 
 	}
--- respaldoDual.sh
+++ respaldoDual.sh
@@ -9,7 +9,7 @@ este=$(pwd | sed 's:.*/::')
 fecha=`date +%Y%m%d_%H%M`
 
 #Comprimir en 7z y guardarlo en carpeta local ( ~/playFramework/respaldos)
-7z a -mx=9 -mmt12 -xr!target ~/playFramework/respaldos/$fecha-resp-$este.7z ${PWD}
+7z a -mx=9 -mmt12 -xr!target -xr!.git ~/playFramework/respaldos/$fecha-resp-$este.7z ${PWD}
 echo "Se ha respaldado en la ruta ~/playFramework/respaldos con el nombre $fecha-resp-$este.7z"
 
 #guardar el comprimido en carpeta para respaldar en la nube (~/respaldosRemotos)
@@ -17,3 +17,10 @@ cp -r ~/playFramework/respaldos/$fecha-resp-$este.7z ~/respaldosRemotos/
 echo "Se ha respaldado en la ruta ~/respaldosRemotos con el nombre $fecha-resp-$este.7z"
 
 #Esta programado un respaldo diario a todo lo que esté en la carpeta respaldosRemotos, con Deja Dump se respalda via webDav en box.com
+
+
+git init
+git add *
+git branch -M main
+git remote add origin https://github.com/epuente/tve.git
+git push https://github.com/epuente/tve.git
